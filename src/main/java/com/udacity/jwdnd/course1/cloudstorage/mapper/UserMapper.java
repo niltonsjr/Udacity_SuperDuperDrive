7bb30e3ffe7ee62b56409f9ef0797a8e5e6913ed
Nilton da Silva Junior <tonsti@gmail.com>
2021-03-27 20:03:41 +0100

    Adding UserMapper

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("SELECT * FROM USERS WHERE username = #{username}")
+    User getUser(String username);
+
+    @Insert("INSERT INTO USERS (username, salt, password, firtname, lastname) " +
+            "VALUES (#{username}, #{salt}, #{password}, #{firstname}, #{lastname})")
+    Integer insert(User user);
+}
