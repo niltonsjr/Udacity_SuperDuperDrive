39545ee95aede59c45deea00a0d92536ae0e8d9a
Nilton da Silva Junior <tonsti@gmail.com>
2021-04-06 20:35:38 +0200

    Adding error pages

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/MyErrorController.java
@@ -0,0 +1,38 @@
+package com.udacity.jwdnd.course1.cloudstorage.config;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+// https://attacomsian.com/blog/spring-boot-custom-error-page
+
+public class MyErrorController implements ErrorController {
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        // get error status
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            // display specific error page
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "500";
+            } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "403";
+            }
+        }
+        // display generic error
+        return "error";
+    }
+}
